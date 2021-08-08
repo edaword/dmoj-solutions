f5635b6d624f4ff611443b246c5d3ee60ddf53db
edaword <edawordwang@gmail.com>
2021-08-08 19:03:08 -0400

    finished cow gymnastics but looked at the sol

--- usaco19decbronze1/src/usaco19decbronze1/Usaco19decbronze1.java
+++ usaco19decbronze1/src/usaco19decbronze1/Usaco19decbronze1.java
@@ -8,45 +8,27 @@ public class Usaco19decbronze1 {
     static StringTokenizer st;
     public static void main(String[] args) throws IOException{
         int k = readInt(), n = readInt();
-        ArrayList<Integer>[] ans = new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            ans[i] = new ArrayList<Integer>();
-        }
-        //for each line
+        boolean[][] ans = new boolean[20][20];
         for (int i = 0; i < k; i++) {
-            //all the numbers in the row
-            int[] nums = new int[n];
+            int[]line = new int[n];
             for (int j = 0; j < n; j++) {
-                nums[j] = readInt();
+                line[j] = readInt();
             }
-            //for each in nums
             for (int j = 0; j < n; j++) {
-                //fill up
-                int idx = nums[j]-1;
-                if (ans[idx].isEmpty()) {
-                    for (int l = j+1; l < n; l++) {
-                        ans[idx].add(nums[l]);
-                    }
-                } else {
-                    ArrayList<Integer> temp = new ArrayList();
-                    for (int l = j+1; l < n; l++) {
-                        temp.add(nums[l]);
-                    }
-                    for (int l = 0; l < ans[idx].size(); l++) {
-                        //gets the number at position l
-                        int x = ans[idx].get(l);
-                        if (!temp.contains(x)) {
-                            ans[idx].remove(l);
-                        }
-                    }
+                for (int l = j+1; l < n; l++) {
+                    ans[line[j]-1][line[l]-1] = true;
                 }
             }
         }
-        int output = 0;
+        int x = 0;
         for (int i = 0; i < n; i++) {
-            output+= ans[i].size();
+            for (int j = i+1; j < n; j++) {
+                if (!ans[i][j] || !ans[j][i]) {
+                    x++;
+                }
+            }
         }
-        System.out.println(output);
+        System.out.println(x);
     }
     
    
