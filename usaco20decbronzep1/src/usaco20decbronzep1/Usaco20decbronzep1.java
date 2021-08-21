b34ce2c92f8e7ac6290c813610acbafbb4524623
edaword <edawordwang@gmail.com>
2021-08-20 23:28:41 -0400

    actually solved this without looking at the solution !!! #progress

--- usaco20decbronzep1/src/usaco20decbronzep1/Usaco20decbronzep1.java
+++ usaco20decbronzep1/src/usaco20decbronzep1/Usaco20decbronzep1.java
@@ -1,21 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package usaco20decbronzep1;
+import java.util.*;
+import java.io.*;
 
-/**
- *
- * @author Edward Wang<https://github.com/edaword>
- */
 public class Usaco20decbronzep1 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException{
+        // a + b + c is biggest
+        long[] nums = new long[7];
+        for (int i = 0; i < 7; i++) nums[i] = readLong();
+        Arrays.sort(nums);
+        long a = nums[0];
+        long b = nums[1];
+        long abc = nums[6];
+        System.out.println(a + " " + b + " " + (abc-a-b));
     }
     
+    static void dbg(String s, int x) {
+//        System.out.println(s + " " + x);
+    }
+
+    static String next () throws IOException {
+            while (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine().trim());
+            return st.nextToken();
+    }
+    static long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt () throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble () throws IOException {
+            return Double.parseDouble(next());
+    }
+    static char readCharacter () throws IOException {
+            return next().charAt(0);
+    }
+    static String readLine () throws IOException {
+            return br.readLine().trim();
+    }
+
 }
